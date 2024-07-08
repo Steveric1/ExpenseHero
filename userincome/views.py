6f64a755b31bb09155db4a8864327599d88c94d3
Eric <stephen.erich243@gmail.com>
2024-07-08 16:28:35 +0000

    updated userincome views

--- userincome/views.py
+++ userincome/views.py
@@ -29,7 +29,10 @@ def index(request):
     paginator = Paginator(income, 3)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    currency = UserPreference.objects.get(user=request.user).currency
+    try:
+        currency = UserPreference.objects.get(user=request.user).currency
+    except UserPreference.DoesNotExist:
+        currency = None
     context = {
         'income': income,
         'page_obj': page_obj,
