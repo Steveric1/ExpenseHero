6aafcc79f5765c149b4e3a3d2c1f48217ea4edea
Eric <stephen.erich243@gmail.com>
2024-07-08 12:52:30 +0000

    updated build.sh file

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 # Exit on error
 set -o errexit
 
+apt-get update && apt-get install -y build-essential libffi-dev
+
+pip install --upgrade pip
+
+pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
 
 python manage.py collectstatic  --no-input
