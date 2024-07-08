83eb863c28c7239154da9ed8032af928c3b13285
Eric <stephen.erich243@gmail.com>
2024-07-08 14:18:14 +0000

    updated settings.py file to setup my database for render in production

--- expensewebsite/settings.py
+++ expensewebsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-28o+p1yr70*qkks35*w8&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "https://expensehero.onrender.com/"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "expensehero.onrender.com"]
 
 
 # Application definition
