095636fb22771d0bbafa30b4a9ebcf26dadd2280
Eric <stephen.erich243@gmail.com>
2024-07-08 13:11:20 +0000

    updated requirements.txt and build.sh to have a successful build on render

--- build.sh
+++ build.sh
@@ -3,9 +3,13 @@
 # Exit on error
 set -o errexit
 
-apt-get update && apt-get install -y build-essential libffi-dev
+# Update and install system dependencies
+apt-get update
+apt-get install -y build-essential libffi-dev
 
-pip install --upgrade pip
+# Clean up
+apt-get clean
+rm -rf /var/lib/apt/lists/*
 
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
