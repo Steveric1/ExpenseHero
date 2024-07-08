2dc48a859d575ae18e0f199280c22734f470705a
Eric <stephen.erich243@gmail.com>
2024-07-08 16:02:47 +0000

    Added script to create superuser for admin user in the production to manage users

--- build.sh
+++ build.sh
@@ -10,3 +10,6 @@ pip install -r requirements.txt
 # Collect static files and run migrations
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+# Create a superuser if it doesn't exist
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='$ADMIN_USERNAME').exists() or User.objects.create_superuser('$ADMIN_USERNAME', '$ADMIN_EMAIL', '$ADMIN_PASSWORD')" | python manage.py shell
