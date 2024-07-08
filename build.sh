8bf5d5331f824fb6a4239c58b29b93d2472ab22a
Eric <stephen.erich243@gmail.com>
2024-07-08 13:19:48 +0000

    build.sh updated

--- build.sh
+++ build.sh
@@ -3,9 +3,6 @@
 # Exit on error
 set -o errexit
 
-# Install system dependencies
-apt-get install --no-install-recommends -y build-essential libffi-dev python3-dev
-
 # Upgrade pip and install dependencies
 pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
